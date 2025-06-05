96188724b1eb8d6db0f852b4622603003bcbc80a
Jay Raval <70907419+jayr1867@users.noreply.github.com>
2025-06-05 15:11:12 +0530

    stage3

--- app/main.py
+++ app/main.py
@@ -9,11 +9,14 @@ def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     client,_ = server.accept()
 
+
+    rec_msg = client.recv(1024)
+
     message_size = (1).to_bytes(4, "big", signed=True)
-    correlation_id = (7).to_bytes(4, "big")
+    correlation_id = rec_msg[8:12]
 
     client.sendall(message_size + correlation_id)
-
+    client.close()
 
 if __name__ == "__main__":
     main()
